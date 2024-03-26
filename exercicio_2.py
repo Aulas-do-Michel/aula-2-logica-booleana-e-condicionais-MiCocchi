43102535c862ed436034514b69362295f71de713
MiCocchi <163673360+MiCocchi@users.noreply.github.com>
2024-03-26 20:19:45 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,6 +1,7 @@
+brca1 = str("chr17")
 cromossomo = str(input("Digite o cromossomo: "))
 intervalo = int(input("Digite a posição: "))
-if cromossomo == str("chr17") and intervalo >= 41196312 and intervalo <= 41277500 :
+if cromossomo == brca1 and intervalo >= 41196312 and intervalo <= 41277500 :
     print("Resposta:")
     print("Sim")
 else:
